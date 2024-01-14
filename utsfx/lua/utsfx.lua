e4c2be2788099486855998880c5b77908b99ac27
Berdan Can Yetkiner <13546853+berdancanyet@users.noreply.github.com>
2024-01-15 02:15:39 +0300

    perfection

--- utsfx/lua/utsfx.lua
+++ utsfx/lua/utsfx.lua
@@ -1,70 +1,65 @@
-local parse = parse
-local msg = msg
+local parse, msg, msg2 = parse, msg, msg2
+local firstblood = "fun/firstblood.wav"
+local humiliation = "fun/humiliation.wav"
+local godlike = "fun/godlike.wav"
+
+function array(length,mode)
+     local array = {}
+     if mode == nil then 
+          mode = 0 
+     end
+     for i = 1,length do 
+          array[i] = mode 
+     end
+     return array
+end
 
 if sample==nil then sample={} end
-sample.ut={}
 
-function initArray(m)
-	local array = {}
-	for i = 1, m do
-		array[i]=0
-	end
-	return array
-end
+sample.ut={
+     level = array(32),
+    fblood = 0
+}
 
+--You can edit and add more levels!
+--COLOR CODE,MESSAGE,SOUND
 levels = {}
-levels[2] = {"173255047","made a Doublekill!","kgb2d/utsfx/doublekill.wav"}
-levels[3] = {"026255525","made a Triplekill!","kgb2d/utsfx/triplekill.wav"}
-levels[4] = {"000206209","made a Multikill!","kgb2d/utsfx/multikill.wav"}
-levels[5] = {"132112255","made a Killingspree!@C","kgb2d/utsfx/killingspree.wav"}
-levels[6] = {"138043226","is Dominating!@C","kgb2d/utsfx/dominating.wav"}
-levels[7] = {"138043226","is UNSTOPPABLE!@C","kgb2d/utsfx/unstoppable.wav"}
-levels[8] = {"138043226","OWNS!@C","kgb2d/utsfx/ownage.wav"}
-levels[9] = {"138043226","made a MO-O-O-O-ONSTERKILL-ILL-ILL!@C","kgb2d/utsfx/monsterkill.wav"}
-levels[10] = {"138043226","GOT ANOTHER ONE@C","kgb2d/utsfx/holyshit.wav"}
-
-sample.ut.timer=initArray(32)
-sample.ut.level=initArray(32)
-sample.ut.fblood=0
-
-addhook("second","sample.ut.second")
-function sample.ut.second()
-	for _, id in pairs(player(0, "table")) do
-		if sample.ut.timer[id] ~= 4 then
-			sample.ut.timer[id] = sample.ut.timer[id] + 1
-		end
-	end
-end
+levels[2] = {"173255047","made a Doublekill!","fun/doublekill.wav"}
+levels[3] = {"026255525","made a Multikill!","fun/multikill.wav"}
+levels[4] = {"000206209","made a Killingspree!@C","fun/killingspree.wav"}
+levels[5] = {"132112255","is on Rampage!@C","fun/rampage.wav"}
+levels[6] = {"138043226","is UNSTOPPABLE!@C","fun/unstoppable.wav"}
+levels[7] = {"138043226","made a Ultrakill!@C","fun/ultrakill.wav"}
+levels[8] = {"138043226","made a MO-O-O-O-ONSTERKILL-ILL-ILL!@C","fun/monsterkill.wav"}
+--levels[9] = {"138043226","made a MO-O-O-O-ONSTERKILL-ILL-ILL!@C","fun/monsterkill.wav"}
 
 addhook("startround","sample.ut.startround")
 function sample.ut.startround()
-	sample.ut.fblood=0
+     sample.ut.fblood=0
+     sample.ut.level=array(32)
 end
 
 addhook("kill","sample.ut.kill")
 function sample.ut.kill(killer,victim,weapon)
-	if sample.ut.timer[killer]>3 then
-		sample.ut.level[killer]=0
-	end
-		level=sample.ut.level[killer]
-		level=level+1
-		sample.ut.level[killer]=level
-		sample.ut.timer[killer]=0
-	if (sample.ut.fblood==0) then
-		sample.ut.fblood=1
-		parse("sv_sound \"kgb2d/utsfx/firstblood.wav\"")
-		msg("\169200000000"..player(killer,"name").. " sheds FIRST BLOOD!@C")
-	end
-	if (weapon==50) then
-		parse("sv_sound \"kgb2d/utsfx/humiliation.wav\"")
-		msg("\169255069000"..player(killer,"name").." humiliated "..player(victim,"name").."!")
-	else
-		if levels[level] then
-			parse("sv_sound \""..levels[level][3].."\"")
-			msg("\169"..levels[level][1]..""..player(killer,"name").." "..levels[level][2])
-		elseif (level > #levels) then
-			parse("sv_sound \"kgb2d/utsfx/godlike.wav\"")
-			msg("\169000255000"..player(killer,"name").." IS G O D L I K E! "..level.." KILLS!@C")
-		end
-	end
+level=sample.ut.level[killer]
+level=level+1
+sample.ut.level[killer]=level
+     if (sample.ut.fblood==0) then
+          sample.ut.fblood=1
+          parse("sv_sound "..firstblood)
+          local message = "\169200000000"..player(killer,"name").." sheds FIRST BLOOD!@C"
+          msg(message)
+     end
+     if (weapon==50) then
+          parse("sv_sound "..humiliation)
+          local message = "\169255069000"..player(killer,"name").." humiliated "..player(victim,"name").."!"
+          msg(message)
+     else
+          local levelData = levels[level] and levels[level] or (level > #levels and {"000255000", "IS GODLIKE! " .. level .. " KILLS!@C", godlike} or nil)
+          if levelData then
+               parse("sv_sound " .. levelData[3])
+               local message = "\169" .. levelData[1] .. "" .. player(killer, "name") .. " " .. levelData[2]
+               msg(message)
+          end
+     end
 end
